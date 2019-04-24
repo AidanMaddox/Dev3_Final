a1ccc7ebd2b6e2a3f2741135a0f2dc15bb53606d
Andrew Hulin <ahulin@usc.edu>
2019-04-24 14:02:06 -0700

    Added a comment to test

--- Dev_Final/ViewController.swift
+++ Dev_Final/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // Andrew's comment in the view controller
 
 }
 

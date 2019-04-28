655a85cc9cce54e46114b2c3756f5dc180eea78e
Aidan Maddox <gennaro.maddox@gmail.com>
2019-04-27 17:29:24 -0700

    Notes Add and Photo Add
    Added swift files for notes add and photo add. Will add photo kit soon

---
+++ Dev_Final/Notes/AddNoteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNoteViewController.swift
+//  Dev_Final
+//
+//  Created by Aidan Maddox on 4/27/19.
+//  Copyright © 2019 Aidan Maddox. All rights reserved.
+//
+
+import UIKit
+
+class AddNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Dev_Final/Notes/NotesModel.swift
@@ -0,0 +1,15 @@
+//
+//  NotesModel.swift
+//  Dev_Final
+//
+//  Created by Aidan Maddox on 4/27/19.
+//  Copyright © 2019 Aidan Maddox. All rights reserved.
+//
+
+import Foundation
+
+class NotesModel{
+    
+    static let model = NotesModel()
+    
+}
